58bfeef51d466d2407f1705b940c59efa957ecfc
ygenest <yvestablet@gmail.com>
2015-06-05 05:46:57 -0400

    add frm

---
+++ src/main/java/jbossews/FormBean.java
@@ -0,0 +1,13 @@
+package jbossews;
+
+public class FormBean {
+	private String symbLst="Abcde";
+
+	public String getSymbLst() {
+		return symbLst;
+	}
+
+	public void setSymbLst(String symbLst) {
+		this.symbLst = symbLst;
+	}
+}
